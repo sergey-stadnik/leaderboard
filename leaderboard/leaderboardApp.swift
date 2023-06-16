c8d91bde727ae9afff6dbf55b9ef239c16088cdf
Sergii Stadnyk <>
2023-06-16 13:29:06 +0300

    initial com

---
+++ leaderboard/leaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  leaderboardApp.swift
+//  leaderboard
+//
+//  Created by Sergii Stadnyk on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct leaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
